c48527b63ef0ff848dd7a72e290fb59ef2288f75
thesadie <thesadie@gmail.com>
2017-05-01 15:12:37 -0700

    created all inputs file, made fry calc function

--- server.R
+++ server.R
@@ -6,46 +6,58 @@ library(readr)
 
 
 source('utils.R')
+source('calc_fry.R')
 
-habitat_adults <- read_rds('data/reach_habitat.rds')
+habitat_adults <- filter(read_rds('data/reach_habitat.rds'), adults > 0)
 territory <- territory_needs()
 
 shinyServer(function(input, output) {
   
-  spawnInput <- reactive({
-      ceiling(filter(habitat_adults, watershed == input$stream_reach)[[4]])
-  })
-  
-  fryInput <- reactive({
-    ceiling(filter(habitat_adults, watershed == input$stream_reach)[[5]])
-  })
-  
-  parrInput <- reactive({
-    ceiling(filter(habitat_adults, watershed == input$stream_reach)[[6]])
-  })
-  
-  adultInput <- reactive({
-    ceiling(filter(habitat_adults, watershed == input$stream_reach)[[3]])
+  allInput <- reactive({
+    filter(habitat_adults, watershed == input$stream_reach)
   })
   
   output$spawn_hab <- renderUI({
     req(input$stream_reach)
-    textInput('spawn', 'Spawning', value = spawnInput()) 
+    textInput('spawn', 'Spawning', value = ceiling(allInput()[[4]]), width = '60px')
   })
-  
+
   output$fry_hab <- renderUI({
     req(input$stream_reach)
-    textInput('fry', 'Fry', value = fryInput()) 
+    textInput('fry', 'Fry', value = ceiling(allInput()[[5]]), width = '60px')
   })
-  
+
   output$parr_hab <- renderUI({
     req(input$stream_reach)
-    textInput('parr', 'Parr', value = parrInput()) 
+    textInput('parr', 'Parr', value = ceiling(allInput()[[6]]), width = '60px')
   })
   
   output$num_adults <- renderUI({
     req(input$stream_reach)
-    textInput('adults', 'Returning Adults', value = adultInput())
+    textInput('adults', 'Returning Adults', value = ceiling(allInput()[[3]]), width = '60px')
   })
+  
+  #TODO- figure out how to get values from adjustable inputs with reactive defaults
+  num_fry <- reactive({
+    req(input$stream_reach)
+    calc_fry(adults = input$adults,
+             retQ = allInput()[[16]],
+             SCDELT = allInput()[[7]],
+             hatch.alloc = allInput()[[11]],
+             TISD = allInput()[[13]],
+             YOLO = allInput()[[12]],
+             p.tempMC2025 = allInput()[[8]],
+             A.HARV = allInput()[[10]],
+             P.scour.nst = allInput()[[9]],
+             egg.tmp.eff = allInput()[[14]],
+             degday = allInput()[[15]],
+             spawn = input$spawn_hab)
+  })
+
+  output$num_fry <- renderText(num_fry())
+  
+  #TODO-once number of fry calculator works, apply territory needs given available habitats
+  
+  #TODO - figure out parr calculator, take number at maximum parr present (month 3?)
   
 })
