cbf361f51a98e9b314f173bf6c836ae8a0d689ae
thesadie <thesadie@gmail.com>
2017-05-03 13:55:03 -0700

    layout pretty good

--- server.R
+++ server.R
@@ -15,13 +15,13 @@ territory <- territory_needs()
 
 shinyServer(function(input, output) {
   
+  #filter results based on selected reach----
   allInput <- reactive({
     req(input$stream_reach)
     filter(habitat_adults, watershed == input$stream_reach)
   })
   
-  
-  #TODO- figure out how to get values from adjustable inputs with reactive defaults
+  #calc number of spawners and resulting fry----
   num_spawn_fry <- reactive({
     calc_num_fish(adults = as.numeric(input$adults),
              retQ = allInput()$retQ,
@@ -38,7 +38,11 @@ shinyServer(function(input, output) {
              order = allInput()$order)
   })
   
-
+  # create text input with default reach values----
+  output$num_adults <- renderUI({
+    textInput('adults', 'Returning Adults', value = ceiling(allInput()$adults), width = '220px')
+  })
+  
   output$spawn_hab <- renderUI({
     textInput('spawn', 'Spawning', value = ceiling(allInput()$spawning), width = '60px')
   })
@@ -47,29 +51,23 @@ shinyServer(function(input, output) {
     textInput('fry', 'Fry', value = ceiling(allInput()$fry), width = '60px')
   })
   
-  output$num_adults <- renderUI({
-    textInput('adults', 'Returning Adults', value = ceiling(allInput()$adults), width = '220px')
-  })
-
+  # print number of spawners and fry
   output$num_fry <- renderText(pretty_num(num_spawn_fry()$fry, 0))
-  
   output$num_spawners <- renderText(pretty_num(num_spawn_fry()$spawners, 0))
   
+  #print habitat needed
   spawn_need <- reactive(pretty_num(num_spawn_fry()$spawners * 6.2 / 4046.86, 2))
   fry_need <- reactive(pretty_num(num_spawn_fry()$fry * territory[[1]] / 4046.86, 2))
   
   output$spawn_hab_need <- renderText(spawn_need())
   output$fry_hab_need <- renderText(fry_need())
   
+  # print available habitat
   output$spawn_hab_available <- renderText(input$spawn)
   output$fry_hab_available <- renderText(input$fry)
   
-  output$spawn_limit <- renderText(ifelse(input$spawn > spawn_need(), 'No', 'Yes'))
-  output$fry_limit <- renderText(ifelse(input$fry > fry_need(), 'No', 'Yes'))
-  
-  
-  #TODO-once number of fry calculator works, apply territory needs given available habitats
-  
-  #TODO - figure out parr calculator, take number at maximum parr present (month 3?)
+  # print habitat limited
+  output$spawn_limit <- renderText(ifelse(as.numeric(input$spawn) < as.numeric(spawn_need()), 'Yes', 'No'))
+  output$fry_limit <- renderText(ifelse(as.numeric(input$fry) < as.numeric(fry_need()), 'Yes', 'No'))
   
 })
--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI(fluidPage(
   # title + branding
   fluidRow(
     column(width = 12, id = 'title',
-           tags$img(src = 'greylogo.png', width = '250px'),
+           tags$img(src = 'greylogo.png', width = '200px'),
            tags$h1('Chinook Carrying Capacity Calculator', id = 'app_name')
     )),
   fluidRow(
@@ -15,22 +15,26 @@ shinyUI(fluidPage(
                   width = '220px'),
       uiOutput('num_adults'),
       tags$h4('Habitat Available (Acres)'),
-      uiOutput('spawn_hab'),
-      uiOutput('fry_hab')
+      div(
+        uiOutput('spawn_hab'),
+        uiOutput('fry_hab')
+      )
     ),
-    column(width = 10, id = 'fish',
-           div(id='spawn_sq',
-               div(tags$img(src = 'spawn.png'), tags$h4('Spawners', style = 'font-weight:bold;')),
+    column(width = 3, class = 'fish',
+           div(id='fish_sq', class = 'fish_sq', 
+               tags$img(src = 'spawn.png'), 
+               tags$h4('Spawners', style = 'font-weight:bold;'),
                div(tags$h5('Total', style = 'font-weight:bold;'), textOutput('num_spawners')),
                div(tags$h5('Available Habitat', style = 'font-weight:bold;'), textOutput('spawn_hab_available')),
                div(tags$h5('Needed Habitat', style = 'font-weight:bold;'), textOutput('spawn_hab_need')),
-               div(tags$h5('Habitat Limited', style = 'font-weight:bold;'), textOutput('spawn_limit'))),
-           div(id='fry_sq',
-               div(tags$img(src = 'fry.png'), tags$h4('Fry', style = 'font-weight:bold;')),
+               div(tags$h5('Habitat Limited', style = 'font-weight:bold;'), textOutput('spawn_limit')))),
+    column(width = 3, class = 'fish', 
+           div(class='fish_sq',
+               tags$img(src = 'fry.png', style='padding-top:17px;'), 
+               tags$h4('Fry', style = 'font-weight:bold;'),
                div(tags$h5('Total', style = 'font-weight:bold;'), textOutput('num_fry')),
                div(tags$h5('Available Habitat', style = 'font-weight:bold;'), textOutput('fry_hab_available')),
                div(tags$h5('Need Habitat', style = 'font-weight:bold;'), textOutput('fry_hab_need')),
-               div(tags$h5('Habitat Limited', style = 'font-weight:bold;'), textOutput('fry_limit')))
-           )
+               div(tags$h5('Habitat Limited', style = 'font-weight:bold;'), textOutput('fry_limit'))))
   )
 ))
